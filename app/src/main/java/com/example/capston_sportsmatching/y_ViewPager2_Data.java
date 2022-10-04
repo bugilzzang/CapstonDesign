8b5096b69763dcb05ca1c8d77b2aae08bca1c7bd
adonelee <akrso520@naver.com>
2022-10-05 00:32:34 +0900

    이영준 10/05

---
+++ app/src/main/java/com/example/capston_sportsmatching/y_ViewPager2_Data.java
@@ -0,0 +1,10 @@
+package com.example.capston_sportsmatching;
+
+public class y_ViewPager2_Data {
+    int color;
+    String title;
+
+    public y_ViewPager2_Data(int color, String title){
+
+    }
+}
