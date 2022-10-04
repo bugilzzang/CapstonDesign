d02e259f30eb5e52099bfd807edf744c9b2a3249
adonelee <akrso520@naver.com>
2022-10-04 11:24:10 +0900

    처음_이영준

---
+++ capston_이영준/app/src/main/java/com/example/sm_loginpage/singup_page.java
@@ -0,0 +1,14 @@
+package com.example.sm_loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class singup_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup_page);
+    }
+}
