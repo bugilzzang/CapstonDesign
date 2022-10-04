dda9571b82698a855d3f2dbf9e0bd3294011da66
adonelee <akrso520@naver.com>
2022-10-05 01:15:47 +0900

    10/5 viewpager

---
+++ app/src/main/java/com/example/capston_sportsmatching/y_ScreenSlidePageFragment.java
@@ -0,0 +1,16 @@
+package com.example.capston_sportsmatching;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.sql.*;
+
+import androidx.fragment.app.Fragment;
+
+public class y_ScreenSlidePageFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+String SQL = "";
+    }
+}
