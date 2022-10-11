66d30766a5bc7a4116b4ff099944ae50f0298a81
adonelee <akrso520@naver.com>
2022-10-11 13:15:09 +0900

    영준 뷰페이저2

--- app/src/main/java/com/example/capstonproject/ysignup_page.java
+++ app/src/main/java/com/example/capstonproject/ysignup_page.java
@@ -2,13 +2,28 @@ package com.example.capstonproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ysignup_page extends AppCompatActivity {
+
+    Button btn_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ysignup_page);
+
+        btn_submit = (Button) findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), yset_exercise_type.class);
+                startActivity(intent);
+            }
+        });
     }
 }
