69c67a61eee15de425c8f26a1829c3f89126548a
adonelee <akrso520@naver.com>
2022-11-07 21:35:45 +0900

    asdf

--- app/src/main/java/com/example/capstonproject/ylogin_page.java
+++ app/src/main/java/com/example/capstonproject/ylogin_page.java
@@ -26,6 +26,15 @@ public class ylogin_page extends AppCompatActivity {
             }
         });
 
+        //서버 통신 테스트
+        String sendmsg = "vision_write";
+        String result = "영준값"; //자신이 보내고싶은 값을 보내시면됩니다
+        try{
+            String rst = new yTask_test(sendmsg).execute(result,"vision_write").get();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 }
