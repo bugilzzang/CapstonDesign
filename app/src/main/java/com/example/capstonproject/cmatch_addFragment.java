1381d9905493cb8faa4cd4cc23d48d0f418f4e98
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2022-11-08 11:07:33 +0900

    조동훈 레이아웃 구성 11/08

---
+++ app/src/main/java/com/example/capstonproject/cmatch_addFragment.java
@@ -0,0 +1,23 @@
+package com.example.capstonproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class cmatch_addFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cmatch_add, container, false);
+    }
+
+}
