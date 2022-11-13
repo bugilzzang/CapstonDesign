a0c9d7f264893b454de2cf22881d9a9e4fcebbd7
adonelee <akrso520@naver.com>
2022-11-13 17:10:18 +0900

    11/13 영준

--- app/src/main/java/com/example/capstonproject/ylogin_page.java
+++ app/src/main/java/com/example/capstonproject/ylogin_page.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ylogin_page extends AppCompatActivity {
     Button goSignupBtn, loginBtn;
-    TextView textview;
+    EditText login_id, login_pw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,34 @@ public class ylogin_page extends AppCompatActivity {
 
         goSignupBtn = (Button) findViewById(R.id.goSignupBtn);
         loginBtn = (Button) findViewById(R.id.loginbtn);
-        TextView textview = (TextView) findViewById(R.id.textView);
+        login_id = (EditText) findViewById(R.id.login_id);
+        login_pw = (EditText) findViewById(R.id.login_pw);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try{
+                    //로그인 시도
+                    yTask task = new yTask("login");
+                    String result =  task.execute("id=" + login_id.getText() + "&pw=" + login_pw.getText()).get();
+
+                    //로그인 결과
+                    if(result.equals("로그인성공")){
+                        Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                    }else if(result.equals("로그인실패")){
+                        Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+
+                    }else{
+                        Toast.makeText(getApplicationContext(), "로그인 오류", Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+                    Log.i("loginpage custom-log",e.getMessage());
+                    Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+/*
                 //서버 통신 테스트
                 String yMSG = "영준값"; //자신이 보내고싶은 값을 보내시면됩니다
                 try{
@@ -35,7 +60,7 @@ public class ylogin_page extends AppCompatActivity {
                 }catch (Exception e){
                     Log.i("loginpage custom-log",e.getMessage());
                     Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
-                }
+                }*/
             }
         });
 
--- app/src/main/java/com/example/capstonproject/ysignup_page.java
+++ app/src/main/java/com/example/capstonproject/ysignup_page.java
@@ -6,17 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class ysignup_page extends AppCompatActivity {
 
-    Button btn_submit;
+    Button confirm_id, confirm_email,btn_submit;
+    EditText user_id, user_pw, user_pw2, user_name, user_email, birth_year, birth_month, birth_day, user_phone;
+    RadioGroup sex_group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ysignup_page);
 
+
+        confirm_id = (Button) findViewById(R.id.confirm_id);
+        confirm_id = (Button) findViewById(R.id.confirm_id);
         btn_submit = (Button) findViewById(R.id.btn_submit);
+        user_id = (EditText) findViewById(R.id.user_id);
+        user_id = (EditText) findViewById(R.id.user_pw);
+        user_id = (EditText) findViewById(R.id.user_pw2);
+        user_id = (EditText) findViewById(R.id.user_name);
+        user_id = (EditText) findViewById(R.id.user_email);
+        user_id = (EditText) findViewById(R.id.birth_year);
+        user_id = (EditText) findViewById(R.id.birth_month);
+        user_id = (EditText) findViewById(R.id.birth_day);
+        user_id = (EditText) findViewById(R.id.user_phonenumber);
+
 
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
