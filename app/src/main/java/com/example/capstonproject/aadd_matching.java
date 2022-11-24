e3a50ffb69374ff065ca8048952a1bb6b1a4a919
Julienjang <115388594+Julienjang@users.noreply.github.com>
2022-11-24 21:02:07 +0900

    221124 addmatching.java fix!

--- app/src/main/java/com/example/capstonproject/aadd_matching.java
+++ app/src/main/java/com/example/capstonproject/aadd_matching.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ActionMenuView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,8 +27,13 @@ public class aadd_matching extends AppCompatActivity {
     chomeFragment chomeFragment;
     cmatch_addFragment cmatch_addFragment;
     cmatchFragment cmatchFragment;
-    cchattingFragment cchattingFragment;
+    EditText match_subject, edittext_persons, edittext_major;
+    Button rg_btn_mercenary, rg_btn_rival, rg_btnsoccer,rg_btnfootball,rg_btnman ,rg_btnfemale;
+    RadioGroup radio_groupsex, radio_groupexercise, radio_group_type;
+    DatePicker match_time;
 
+
+    String str_sex, str_excercise, str_type;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,7 +43,68 @@ public class aadd_matching extends AppCompatActivity {
         SharedPreferences USERINFO = getSharedPreferences("USERINFO", MODE_PRIVATE); //영준
         //처음화면
 
+
         NavigationBarView navigationBarView = findViewById(R.id.bottomNavi);
+        //aad_matching 버튼 연결
+        match_subject = (EditText) findViewById(R.id.match_subject);
+        edittext_persons=(EditText) findViewById(R.id.edittext_persons);
+        edittext_major =(EditText) findViewById(R.id.edittext_major);
+        rg_btn_mercenary= (Button) findViewById(R.id.rg_btn_mercenary);
+        rg_btn_rival = (Button) findViewById(R.id.rg_btn_rival);
+        rg_btnsoccer = (Button) findViewById(R.id.rg_btnsoccer);
+        rg_btnfootball = (Button) findViewById(R.id.rg_btnfootball);
+        rg_btnman = (Button) findViewById(R.id.rg_btnman);
+        rg_btnfemale = (Button) findViewById(R.id.rg_btnfemale);
+        radio_groupsex = (RadioGroup) findViewById(R.id.radio_groupsex);
+        radio_groupexercise = (RadioGroup) findViewById(R.id.radio_groupexercise);
+        radio_group_type = (RadioGroup) findViewById(R.id.radio_group_type);
+        match_time=(DatePicker) findViewById(R.id.match_time);
+
+
+        radio_groupsex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rg_btnman:
+                        str_sex = "male";
+                        break;
+                    case R.id.rg_btnfemale:
+                        str_sex = "female";
+                        break;
+                }
+            }
+        });
+
+        radio_groupexercise.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rg_btnsoccer:
+                        str_excercise = "soccer";
+                        break;
+                    case R.id.rg_btnfootball:
+                        str_excercise = "football";
+                        break;
+                }
+            }
+        });
+
+        radio_group_type.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rg_btn_mercenary:
+                        str_type = "merecenary";
+                        break;
+                    case R.id.rg_btn_rival:
+                        str_type = "rival";
+                        break;
+                }
+            }
+        });
+
+
+
 
         //바텀 네비게이션뷰 안의 아이템 설정
         navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
